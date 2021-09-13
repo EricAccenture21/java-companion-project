5a1faf371e09d1ebc9bd7cf55bc8f5b55607f971
eric.neil <eric.neil@AFSWWAVUQ6QR1H.us.accenturefederal.com>
2021-09-13 09:55:10 -0500

    recommit from last commit

--- mgl-webapp_task1-0/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
+++ mgl-webapp_task1-0/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
@@ -1,4 +1,4 @@
- package com.organization.mvcproject.service;
+  package com.organization.mvcproject.service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,37 +10,10 @@ import com.organization.mvcproject.model.Game;
 @Service
 public class GameServiceImpl implements GameService {
 
-	private static Long gameId = new Long(0);
-	private static Long companyId = new Long(0);
-	private static List<Game> games = new ArrayList<Game>();
+		@Autowired
+		private MockGmaeDAC mockGameDAC; 
+		
 
-	static {
-		games = populateGames();
-	}
-
-	private static List<Game> populateGames() {
-
-		Game game1 = new Game();
-		game1.setId(++gameId);
-		game1.setGenre("Sport");
-		game1.setName("Rocket League");
-
-		Game game2 = new Game();
-		game2.setId(++gameId);
-		game2.setGenre("Shooter");
-		game2.setName("Halo 3");
-
-		Game game3 = new Game();
-		game3.setId(++gameId);
-		game3.setGenre("MMORPG");
-		game3.setName("Runescape");
-
-		games.add(game1);
-		games.add(game2);
-		games.add(game3);
-
-		return games;
-	}
 
 	@Override
 	public List<Game> retrieveAllGames() {
@@ -49,36 +22,11 @@ public class GameServiceImpl implements GameService {
 
 	@Override
 	public Game saveGame(Game game) {
-		game.setId(++gameId);
-		games.add(game);
-		return game;
+		return mockGameDAC.saveGame(game);
 	}
 	
-
-	public static Long getGameId() {
-		return gameId;
+	public boolean deleteGame(Long gameID) {
+		return 
 	}
-
-	public static void setGameId(Long gameId) {
-		GameServiceImpl.gameId = gameId;
-	}
-
-	public static Long getCompanyId() {
-		return companyId;
-	}
-
-	public static void setCompanyId(Long companyId) {
-		GameServiceImpl.companyId = companyId;
-	}
-
-	public static List<Game> getGames() {
-		return games;
-	}
-
-	public static void setGames(ArrayList<Game> games) {
-		GameServiceImpl.games = games;
-	}
-	
-	
 
 }
